b200f7a8ada721fac311a9d6cc961c8a0f860fe2
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-02-05 22:11:25 +0000

    stitches component started

---
+++ client/src/components/StitchesCollection.jsx
@@ -0,0 +1,30 @@
+import BO from '../assets/stitches/BO.svg'
+import C2B from '../assets/stitches/C2B.svg'
+import C2F from '../assets/stitches/C2F.svg'
+import C4B from '../assets/stitches/C4B.svg'
+import C4F from '../assets/stitches/C4F.svg'
+import CO from '../assets/stitches/CO.svg'
+import k from '../assets/stitches/k.svg'
+import k2tog from '../assets/stitches/k2tog.svg'
+import m1l  from '../assets/stitches/m1l.svg'
+import m1pl from '../assets/stitches/m1pl.svg'
+import m1pr from '../assets/stitches/m1pr.svg'
+import m1r from '../assets/stitches/m1r.svg'
+import noStitch from '../assets/stitches/noStitch.svg'
+import p from '../assets/stitches/p.svg'
+import p2tog from '../assets/stitches/p2tog.svg'
+import p2togTbl from '../assets/stitches/p2togTbl.svg'
+import pm from '../assets/stitches/pm.svg'
+import sm from '../assets/stitches/sm.svg'
+import ssk from '../assets/stitches/ssk.svg'
+import yo from '../assets/stitches/yo.svg'
+
+export default function StitchesCollection (){
+    const stitches = [
+        {title:'Bind Off', abreviation:'BO', category:'Miscellaneous', logo:BO},
+    ]
+    return(
+        <>
+        </>
+    )
+}
